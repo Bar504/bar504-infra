f908c561285ce31cf8fd9f1a24ab6dd6f7ec60b0
clauto-code <claude.you@protonmail.com>
2026-05-02 16:41:21 +0900

    feat: add Route53 A record for proxy.bar504.net

---
+++ claude-proxy.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "claude_proxy" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "proxy.${var.domain_name}"
+  type    = "A"
+  ttl     = 300
+  records = [var.claude_proxy_ip_address]
+}
